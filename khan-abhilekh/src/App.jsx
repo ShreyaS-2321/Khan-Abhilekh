937d190f9b632040414c3581c6ec3c8efbca16f2
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-02-08 06:49:30 +0530

    Update App.jsx

--- khan-abhilekh/src/App.jsx
+++ khan-abhilekh/src/App.jsx
@@ -4,15 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShiftLogForm from "./components/shiftform";
 import Login from "./components/login";
 import Signup from "./components/signup";
+import Dashboard from "./components/dashboard";
+import ShiftLogTable from "./components/shiftdata";
+import Landing from "./components/Landing";
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Define Routes for Login and Signup */}
-        <Route path="/" element={<ShiftLogForm/>}/>
+        <Route path="/shiftform" element={<ShiftLogForm/>}/>
+        <Route path="/" element={<Landing/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/table" element={<ShiftLogTable/>}/>
       </Routes>
     </Router>
   );
