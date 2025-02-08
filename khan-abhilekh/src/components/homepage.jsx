e48aa3d87a45a7da99284c60e4715681ca273633
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-02-09 02:02:34 +0530

    Update homepage.jsx

--- khan-abhilekh/src/components/homepage.jsx
+++ khan-abhilekh/src/components/homepage.jsx
@@ -49,13 +49,13 @@ export default function Homepage() {
       <div className="flex-1 flex flex-col">
         {/* Navbar */}
         <div className="flex justify-end bg-white p-4 shadow-md gap-4">
-        <div className=" cursor-pointer flex items-center gap-2 text-white rounded-lg bg-red-500 px-4 py-2 hover:bg-red-600">
+        <Link to="/alertlogs" className=" cursor-pointer flex items-center gap-2 text-white rounded-lg bg-red-500 px-4 py-2 hover:bg-red-600">
             <Siren size={30} />
-          </div>
+          </Link>
           <Link to ="/dashboard" className=" cursor-pointer flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg">Dashboard</Link>
-          <button className=" cursor-pointer flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
+          <Link to="/alert" className=" cursor-pointer flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
             <BellPlus size={20} /> Create Alert
-          </button>
+          </Link>
         </div>
 
         {/* Shift Blocks */}
